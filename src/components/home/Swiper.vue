f49385b9c8c3266477cebbf80d2483c8c129b217
QQCR <378626360@qq.com>
2020-05-01 23:21:24 +0800

    change

---
+++ src/components/home/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true
+      },
+      swiperList:[{
+        id: "0001",
+        imgUrl:"http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg"
+      },{
+        id: "0002",
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1804/c4/1cdd28811593b802.jpg_750x200_5fbb7c91.jpg"
+      }]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff !important
+.wrapper
+  height:0
+  width: 100%
+  overflow: hidden
+  padding-bottom: 26.67%
+  background: #eee
+  .swiper-img
+    width:100%
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from "@/components/home/Header";
+import HomeSwiper from "@/components/home/Swiper";
 export default {
   name: "Home",
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 };
 </script>
