f097b90e342f91827330136637cc8ed898cf7a6c
QQCR <378626360@qq.com>
2020-06-16 23:08:20 +0800

    detail finish

--- src/components/detail/Banner.vue
+++ src/components/detail/Banner.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg" />
+      <img class="banner-img" :src="bannerImg" />
       <div class="banner-info">
-        <div class="banner-title">大连圣亚海洋世界(AAAA景区)</div>
+        <div class="banner-title">{{signtName}}</div>
         <div class="banner-number">
           <span class="iconfont banner-icon">&#xe635;</span>
-          39
+          {{this.gallaryImgs.length}}
         </div>
       </div>
     </div>
-    <common-gallery :imgs="imgs" v-show="showGallery" @close="closeGallery"></common-gallery>
+    <common-gallery :imgs="gallaryImgs" v-show="showGallery" @close="closeGallery"></common-gallery>
   </div>
 </template>
 
@@ -24,11 +24,12 @@ export default {
   data () {
     return {
       showGallery: false,
-      imgs: [
-        "http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg",
-        "http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png"
-      ]
     }
+  },
+  props: {
+    signtName: String,
+    bannerImg: String,
+    gallaryImgs: Array
   },
   methods: {
     handleBannerClick () {
